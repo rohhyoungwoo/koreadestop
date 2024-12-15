a8ac738d8420ce7a7d3137910808734830504941
rohhyoungwoo <nungma97@naver.com>
2024-12-16 00:30:35 +0900

    241216 파일저장

--- day17/src/task/LottoSet.java
+++ day17/src/task/LottoSet.java
@@ -1,5 +1,28 @@
 package task;
 
+import java.util.HashSet;
+
 public class LottoSet {
+	public static void main(String[] args) {
+		
+//		HashSet<Random> lotto = new HashSet<>();
+//		System.out.println(lotto);
+//		System.out.println(set.toString());
+		
+//		Random lottonum = new Random(((int)(Math.random()*45)+1));
+		
+//		set.add(lotto);
+//		set.add(lottonum);
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
 
 }
